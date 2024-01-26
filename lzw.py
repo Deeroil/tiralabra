f518b3fde4b9341d09bdb17264ead8d2181c6279
Miina Saroma <miina.saroma@helsinki.fi>
2024-01-26 14:39:33 +0200

    Clean up LZW a bit

--- lzw.py
+++ lzw.py
@@ -1,6 +1,5 @@
 import string
 
-
 # https://www.geeksforgeeks.org/lzw-lempel-ziv-welch-compression-technique/
 # https://www2.cs.duke.edu/csed/curious/compression/lzw.html
 
@@ -18,89 +17,16 @@ def lzw_compression(decompressed):
     if s + ch in string_table:
       s = s + ch
     else:
-      # encode p to output file
-      # code = string_table.index(p)
       output_file.append(string_table.index(s))
       string_table.append(s+ch)
       s  = ch
     index += 1
   
   output_file.append(string_table.index(s))
+  return output_file
 
-  # print("table", string_table)
-  # print("output", output_file)
-  return (string_table, output_file)
-
-def lzw_decompression(compressed, string_table):
-  # lue indeksi, etsi se dictionarystä, output substring joka indeksissä.
-  # first char of this substring is concat to working string
-  # this new concat is added to the dict
-  #decoded string becomes the current working string
-  
-  # print(compressed, string_table)
-
-  # string_table = ['a', 'b', 'd', 'n', '_']
-  string_table = [chr(i) for i in range(256)]
-  working_string = ''
-  prevcode = -1
-  currcode = -1
-
-  prevcode = compressed[0]
-  #???????? decode/output prevcode??
-  working_string += string_table[prevcode] #näin???
-
-  index = 0 #tai 1???
-  while index < len(compressed):
-    # print(compressed[index])
-    currcode = compressed[index]
-    entry = string_table[currcode]
-    working_string += entry
-    ch = entry[0]
-
-    # lisää string_table(prevcode)+ch to dictionary???
-    prevcode = currcode
-
-    index += 1
-
-
-
-def lzw_decompression2(compressed, string_table):
-  numbers = string_table
-  output_file = []
-  # string_table = ['a', 'b', 'd', 'n', '_']
-  string_table = [chr(i) for i in range(256)]
-  print(len(string_table))
-  # print(compressed)
-  entry = ''
-  ch = ''
-  prevcode = -1
-  currcode = -1
-
-  prevcode = compressed[0]
-
-  index = 0
-  while index < len(compressed):
-    currcode = compressed[index]
-    entry = currcode
-    output_file.append(entry)
-    ch = entry[0]
-    string_table.append(prevcode + ch)
-    prevcode = currcode
-
-    index += 1
-  
-  # print("output:", output_file)
-  # print("str_table", string_table)
-
-  decompressed = ''
-  for i in numbers: #venaa miks tää riittää, eiks mun pitäis hyödyntää sitä myöhempää?
-    decompressed += output_file[i]
-
-  return decompressed
-
-
-def lzw_decompression3(compressed):
-  print("compr", compressed)
+def lzw_decompression(compressed):
+  # print("compr", compressed)
   output_file = []
   string_table = [chr(i) for i in range(256)]
   entry = ''
@@ -111,16 +37,16 @@ def lzw_decompression3(compressed):
   index = 1
   while index < len(compressed):
     currcode = compressed[index]
-    print("currcode", currcode)
+    # print("currcode", currcode)
     entry = string_table[currcode]
-    print("entr",entry)
+    # print("entr",entry)
     output_file.append(entry)
     ch = entry[0]
     string_table.append(string_table[prevcode] + ch)
     prevcode = currcode
 
     index += 1
-  print("opfil", output_file)
+  # print("opfil", output_file)
   
   decompressed = ''
   for i in compressed:
@@ -133,8 +59,8 @@ if __name__ == "__main__":
   test_string = "banana_bandana"
   test_string = "kokkola kokakola koko kokko lakko"
   compr = lzw_compression(test_string)
-  print("")
-  output = lzw_decompression3(compr[1])
+  output = lzw_decompression(compr)
+  
   print("compressed:", compr)
   print("after decomp:", output)
   if test_string == output:
