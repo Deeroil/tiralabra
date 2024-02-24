bdb26d9d4e1ed62d8cea74dfeab8cd8fe62c9c85
Miina Saroma <miina.saroma@helsinki.fi>
2024-02-24 13:28:13 +0200

    Comment out file saving tests for now

--- src/tests/huffman_test.py
+++ src/tests/huffman_test.py
@@ -73,7 +73,7 @@ class TestHuffman(unittest.TestCase):
         decompr = self.huffman.decompression(compr)
         self.assertEqual(lorem_longer, decompr)
 
-    def test_huffman_saves_to_file_and_then_decompresses_from_it_right(self):
-        self.huffman.compress_to_file(lorem_ipsum, "src/tests/test_generated/lorem")
-        decompr = self.huffman.decompress_from_file("src/tests/test_generated/lorem")
-        self.assertEqual(lorem_ipsum, decompr)
+    # def test_huffman_saves_to_file_and_then_decompresses_from_it_right(self):
+    #     self.huffman.compress_to_file(lorem_ipsum, "src/tests/test_generated/lorem")
+    #     decompr = self.huffman.decompress_from_file("src/tests/test_generated/lorem")
+    #     self.assertEqual(lorem_ipsum, decompr)
--- src/tests/lzw_test.py
+++ src/tests/lzw_test.py
@@ -69,10 +69,10 @@ class TestLZW(unittest.TestCase):
         decompr = self.lzw.decompression(compr)
         self.assertEqual(customers100, decompr)
 
-    def test_lzw_saves_to_file_and_then_decompresses_from_it_right(self):
-        self.lzw.compress_to_file(lorem_ipsum, "src/tests/test_generated/lorem")
-        decompr = self.lzw.decompress_from_file("src/tests/test_generated/lorem")
-        self.assertEqual(lorem_ipsum, decompr)
+    # def test_lzw_saves_to_file_and_then_decompresses_from_it_right(self):
+    #     self.lzw.compress_to_file(lorem_ipsum, "src/tests/test_generated/lorem")
+    #     decompr = self.lzw.decompress_from_file("src/tests/test_generated/lorem")
+    #     self.assertEqual(lorem_ipsum, decompr)
 
     # def test_book_compresses_and_decompresses(self):
     #     # add some longer text here and test how much it compresses
